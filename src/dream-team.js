ec1ac89b560fed6f40fea94e95ed1c58ea9f3606
Dima Kashko <kashko_di@mail.ru>
2020-10-27 00:46:49 +0300

    feat: count-cats & dream-team task done

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!(members instanceof Array)) return false;
+  let dreamTeam = [];
+  members.forEach((element) => {
+    if (typeof element === "string") dreamTeam.push(element.trim().charAt(0).toUpperCase());
+});
+  return dreamTeam.sort().join('');
 };
